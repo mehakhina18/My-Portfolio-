a73e68b333dcb3f20c79983ed06da3229918626d
mehakhina18 <mehakhina18@gmail.com>
2026-07-23 09:39:32 -0700

    Create App component with Navbar and Hero

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="bg-black text-white overflow-x-hidden">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
