dac5a8cd9402e26463802c9967e27befc8bea394
mehakhina18 <mehakhina18@gmail.com>
2026-07-23 10:21:34 -0700

    Add CustomCursor component to App

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import Loader from "./components/Loader";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import CustomCursor from "./components/CustomCursor";
 
 function App() {
   return (
     <>
       <Loader />
+      <CustomCursor />
 
       <div className="bg-black text-white overflow-x-hidden">
         <Navbar />
